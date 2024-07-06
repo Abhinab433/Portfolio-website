a6df7fadebde1e7e7af57a5052e550b0d879168c
Abhinab Shrestha <93916204+Abhinab433@users.noreply.github.com>
2024-07-06 17:29:30 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 AOS.init({
     once: false
 });
+ const imgElement = document.getElementById('imageChange');
+
+    imgElement.addEventListener('mouseover', () => {
+        imgElement.src = './assets/images/tesst.jpg'; // Replace with your hover image path
+    });
+
+    imgElement.addEventListener('mouseout', () => {
+        imgElement.src = './assets/images/cme.jpg'; // Original image path
+    });
